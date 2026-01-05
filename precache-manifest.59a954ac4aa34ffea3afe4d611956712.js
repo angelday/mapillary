b1a0f25db8e41e6f825146690c9b9f8f0109613a
József Schaffer <sasvari@gmail.com>
2026-01-05 08:37:04 +0100

    Updates

---
+++ precache-manifest.59a954ac4aa34ffea3afe4d611956712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2a0930d324bada32bedce04a445045",
+    "url": "/mapillary/index.html"
+  },
+  {
+    "revision": "7b96697680af63534d72",
+    "url": "/mapillary/static/css/main.01ae0d24.chunk.css"
+  },
+  {
+    "revision": "2fd4c56a6d24eb6fec4a",
+    "url": "/mapillary/static/js/2.facf22a1.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/mapillary/static/js/2.facf22a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b96697680af63534d72",
+    "url": "/mapillary/static/js/main.44e6ae5b.chunk.js"
+  },
+  {
+    "revision": "8d16c5d6b95f0ffd9744",
+    "url": "/mapillary/static/js/runtime-main.7c228276.js"
+  }
+]);
